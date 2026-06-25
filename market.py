f9460932f44306ad4b449abc607adecb6e091a46
swishah <kaliuman@gmail.com>
2026-06-25 22:18:19 +0200

    Create market.py

---
+++ market.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def run_module():
+    st.title("📊 PickPivot: Global Market Scanner")
+    st.info("Pomyślnie załadowano moduł giełdowy!")
+    
+    # Skopiuj tutaj swój dawny kod skanera z pliku pickpivot_global_scanner.py
+    # Zwróć uwagę, aby zachować odpowiednie wcięcia.
+    st.write("Wklej zawartość analizy yfinance tutaj.")
